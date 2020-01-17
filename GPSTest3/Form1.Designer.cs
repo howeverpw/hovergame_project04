8f32c833eee7dc7c117ae812cfca60c8822a934f
alikeman05 <30924952+alikeman05@users.noreply.github.com>
2020-01-17 20:31:19 +0000

    Initial commit

---
+++ GPSTest3/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace GPSTest3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gmap = new GMap.NET.WindowsForms.GMapControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.LatText = new System.Windows.Forms.TextBox();
+            this.LongText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.RangeText = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.GPSOutputText = new System.Windows.Forms.RichTextBox();
+            this.GenerateButton = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // gmap
+            // 
+            this.gmap.Bearing = 0F;
+            this.gmap.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.gmap.CanDragMap = false;
+            this.gmap.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.gmap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gmap.GrayScaleMode = false;
+            this.gmap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gmap.LevelsKeepInMemory = 5;
+            this.gmap.Location = new System.Drawing.Point(23, 24);
+            this.gmap.MarkersEnabled = true;
+            this.gmap.MaxZoom = 18;
+            this.gmap.MinZoom = 3;
+            this.gmap.MouseWheelZoomEnabled = false;
+            this.gmap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gmap.Name = "gmap";
+            this.gmap.NegativeMode = false;
+            this.gmap.PolygonsEnabled = true;
+            this.gmap.RetryLoadTile = 0;
+            this.gmap.RoutesEnabled = true;
+            this.gmap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Fractional;
+            this.gmap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gmap.ShowTileGridLines = false;
+            this.gmap.Size = new System.Drawing.Size(1366, 959);
+            this.gmap.TabIndex = 0;
+            this.gmap.Zoom = 0D;
+            this.gmap.MouseClick += new System.Windows.Forms.MouseEventHandler(this.gmap_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1412, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(470, 67);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Set Position (GPS)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LatText
+            // 
+            this.LatText.Location = new System.Drawing.Point(1412, 153);
+            this.LatText.Name = "LatText";
+            this.LatText.Size = new System.Drawing.Size(220, 31);
+            this.LatText.TabIndex = 2;
+            this.LatText.Text = "37.8651";
+            // 
+            // LongText
+            // 
+            this.LongText.Location = new System.Drawing.Point(1662, 153);
+            this.LongText.Name = "LongText";
+            this.LongText.Size = new System.Drawing.Size(220, 31);
+            this.LongText.TabIndex = 3;
+            this.LongText.Text = "-119.5383";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1412, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "GPS Lat";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1657, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "GPS Long";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1412, 292);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(176, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Zoom Radius (m)";
+            // 
+            // RangeText
+            // 
+            this.RangeText.Location = new System.Drawing.Point(1412, 323);
+            this.RangeText.Name = "RangeText";
+            this.RangeText.Size = new System.Drawing.Size(145, 31);
+            this.RangeText.TabIndex = 6;
+            this.RangeText.Text = "1000";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label4.Location = new System.Drawing.Point(1552, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 44);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Controls";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1412, 916);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(470, 67);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Clear Waypoints";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // GPSOutputText
+            // 
+            this.GPSOutputText.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GPSOutputText.Location = new System.Drawing.Point(1412, 614);
+            this.GPSOutputText.Name = "GPSOutputText";
+            this.GPSOutputText.Size = new System.Drawing.Size(470, 296);
+            this.GPSOutputText.TabIndex = 18;
+            this.GPSOutputText.Text = "";
+            // 
+            // GenerateButton
+            // 
+            this.GenerateButton.Location = new System.Drawing.Point(1651, 292);
+            this.GenerateButton.Name = "GenerateButton";
+            this.GenerateButton.Size = new System.Drawing.Size(231, 67);
+            this.GenerateButton.TabIndex = 19;
+            this.GenerateButton.Text = "Generate Zone";
+            this.GenerateButton.UseVisualStyleBackColor = true;
+            this.GenerateButton.Click += new System.EventHandler(this.ZoneButton_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1412, 371);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(195, 25);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Beacon Radius (m)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1412, 399);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 31);
+            this.textBox1.TabIndex = 22;
+            this.textBox1.Text = "750";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(1894, 1009);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.GenerateButton);
+            this.Controls.Add(this.GPSOutputText);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.RangeText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LongText);
+            this.Controls.Add(this.LatText);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gmap);
+            this.Name = "Form1";
+            this.Text = "HoverGames GPS Generator";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gmap;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox LatText;
+        private System.Windows.Forms.TextBox LongText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox RangeText;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.RichTextBox GPSOutputText;
+        private System.Windows.Forms.Button GenerateButton;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ GPSTest3/Form1.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.MapProviders;
+
+
+
+namespace GPSTest3
+{
+
+    public partial class Form1 : Form
+    {
+        //Initialise variables used in the form
+        int RangeInt;
+        double LatDbl = 37.8651;
+        double LongDbl = -119.5383;
+        string LatStr;
+        string LongStr;
+        string RangeStr;
+        int ZoomInt;
+        double GPSLatMin = 180;
+        double GPSLatMax = -180;
+        double GPSLngMin = 180;
+        double GPSLngMax = -180;
+        double GPSLatDroneCount;
+        double GPSLngDroneCount;
+        double GPSLngDroneCountDn;
+        double GPSLatDroneCountDn;
+        double GPSLatRes;
+        double GPSLngRes;
+        double GPSinM = 111.32;
+        double DroneRange;
+        bool ready = false;
+        double count;
+        double LineLength;
+
+        //Map Waypoints and drawing grid area overlay names
+        GMapOverlay markers = new GMapOverlay("markers");
+        GMapOverlay polygons = new GMapOverlay("polygons");
+
+        //Lists used throughout the code
+        List<PointLatLng> DrawnCoordinates = new List<PointLatLng>();
+        List<PointLatLng> AllGridPoints = new List<PointLatLng>();
+        List<PointLatLng> GridPointsInsideOperatingArea = new List<PointLatLng>();
+
+        //Defined points used for user defined functions
+        PointLatLng CheckPoint0;
+        PointLatLng CheckPoint1;
+        PointLatLng CheckPoint2;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            gmap.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+            gmap.Position = new GMap.NET.PointLatLng(37.8651, -119.5383);
+            gmap.Zoom = 14;
+            gmap.ShowCenter = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Setup the map to go to the desired position based on text box entries
+            LatStr = LatText.Text;
+            LongStr = LongText.Text;
+            LatDbl = Convert.ToDouble(LatStr);
+            LongDbl = Convert.ToDouble(LongStr);
+            RangeStr = RangeText.Text;
+            RangeInt = Convert.ToInt32(RangeStr);
+
+            //Set the zoom to a range suitable for the operating distance
+            if (RangeInt >= 10000)
+                ZoomInt = 8;
+            if (RangeInt <= 10000)
+                ZoomInt = 10;
+            if (RangeInt <= 5000)
+                ZoomInt = 12;
+            if (RangeInt <= 2500)
+                ZoomInt = 12;
+            if (RangeInt <= 1000)
+                ZoomInt = 14;
+            if (RangeInt <= 500)
+                ZoomInt = 16;
+            if (RangeInt <= 250)
+                ZoomInt = 17;
+            if (RangeInt <= 100)
+                ZoomInt = 18;
+
+            //Configure the map module
+            gmap.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+            gmap.Position = new GMap.NET.PointLatLng(LatDbl, LongDbl);
+            gmap.Zoom = ZoomInt;
+            gmap.ShowCenter = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Clear all button press
+            //Reset all values back to default
+            markers.Markers.Clear();
+            GPSOutputText.Text = "";
+            DrawnCoordinates.Clear();
+            polygons.Polygons.Clear();
+            GPSLatMin = 180;
+            GPSLatMax = -180;
+            GPSLngMin = 180;
+            GPSLngMax = -180;
+            AllGridPoints.Clear();
+            GridPointsInsideOperatingArea.Clear();
+        }
+
+        private void gmap_MouseClick(object sender, MouseEventArgs e)
+        {
+            //Log a coordinate point when clicking on the map
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                double lat = gmap.FromLocalToLatLng(e.X, e.Y).Lat;
+                double lng = gmap.FromLocalToLatLng(e.X, e.Y).Lng;
+
+                //Place a marker on the map when clicked
+                AddMarker(lat, lng);
+
+            }
+        }
+
+
+        private void ZoneButton_Click(object sender, EventArgs e)
+        {
+            DroneRange = (Convert.ToDouble(textBox1.Text)) / 1000;
+            //Draw polygons around the points generated
+            GMapPolygon polygon = new GMapPolygon(DrawnCoordinates, "Operating Area");
+            polygons.Polygons.Add(polygon);
+
+            //Clear all the markers drawn as a shape has now been drawn around the area
+            markers.Markers.Clear();
+
+            //Generate the min and max lat and lng points of all the points listed
+            foreach (PointLatLng point in DrawnCoordinates)
+            {
+                if (GPSLatMax <= point.Lat)
+                {
+                    GPSLatMax = point.Lat;
+                }
+                if (GPSLatMin >= point.Lat)
+                {
+                    GPSLatMin = point.Lat;
+                }
+                if (GPSLngMax <= point.Lng)
+                {
+                    GPSLngMax = point.Lng;
+                }
+                if (GPSLngMin >= point.Lng)
+                {
+                    GPSLngMin = point.Lng;
+                }
+
+            }
+
+            //Draw a box around the operating area
+            List<PointLatLng> points = new List<PointLatLng>();
+            points.Add(new PointLatLng(GPSLatMax, GPSLngMax));
+            points.Add(new PointLatLng(GPSLatMax, GPSLngMin));
+            points.Add(new PointLatLng(GPSLatMin, GPSLngMin));
+            points.Add(new PointLatLng(GPSLatMin, GPSLngMax));
+
+            gmap.Overlays.Add(polygons);
+
+            GPSLatDroneCount = ((GPSLatMax - GPSLatMin) * GPSinM) / DroneRange;
+            GPSLngDroneCount = ((GPSLngMax - GPSLngMin) * GPSinM) / DroneRange;
+
+            GPSLatDroneCountDn = Math.Ceiling(GPSLatDroneCount);
+            GPSLngDroneCountDn = Math.Ceiling(GPSLngDroneCount);
+
+            GPSLatRes = (GPSLatMax - GPSLatMin) / GPSLngDroneCount;
+            GPSLngRes = (GPSLngMax - GPSLngMin) / GPSLatDroneCount;
+
+
+
+            for (int countlat = 0; countlat <= GPSLngDroneCountDn; countlat++)
+            {
+                for (int countlng = 0; countlng <= GPSLatDroneCountDn; countlng++)
+                {
+
+                    AllGridPoints.Add(new PointLatLng((GPSLatMin + (0.0001) + (GPSLatRes * countlat)), (GPSLngMin + (GPSLngRes * countlng))));
+                }
+
+
+            }
+
+            //Used to ensure that functions are not called unless all 3 checkpoints are populated.
+            ready = false;
+
+            foreach (PointLatLng ncoord in DrawnCoordinates)
+            {
+                
+                if (count == 0)
+                {
+                    //Store GPS coordinate point 1
+                    CheckPoint0 = ncoord;
+                    count++;
+                }
+                else if (count == 1)
+                {
+                    //Store GPS coordinate point 2
+                    CheckPoint1 = ncoord;
+                    count++;
+
+                }
+                else if (count == 2)
+                {
+                    //Store GPS coordinate point 3
+                    CheckPoint2 = ncoord;
+                    count = 1;
+                    ready = true;
+                }
+                    if (ready == true)
+                {
+                    foreach (PointLatLng pointsin in AllGridPoints)
+                    {
+
+                        if (PointChecker(CheckPoint0, CheckPoint1, CheckPoint2, pointsin) == true)
+                        {
+                            GridPointsInsideOperatingArea.Add(pointsin);
+                        }
+
+                    }
+                }
+
+
+                
+                
+
+            }
+
+            foreach (PointLatLng ncoordinate in GridPointsInsideOperatingArea)
+            {
+                //Output all the GPS coordinates to the text box
+                GPSOutputText.Text += Environment.NewLine + ncoordinate;
+                //AddMarker(ncoordinate.Lat, ncoordinate.Lng);
+            }
+
+
+            gmap.Position = new GMap.NET.PointLatLng(LatDbl, LongDbl);
+        }
+
+        public void AddMarker(double lat1, double lng1)
+        {
+            //function used to add a marker to the map with coordinates of (lat1, lng1)
+            DrawnCoordinates.Add(new PointLatLng(lat1, lng1));
+            GMapMarker marker = new GMarkerGoogle(
+                new PointLatLng(lat1, lng1),
+                GMarkerGoogleType.red_pushpin);
+            markers.Markers.Add(marker);
+            gmap.Overlays.Add(markers);
+            gmap.Position = new GMap.NET.PointLatLng(LatDbl, LongDbl);
+        }
+
+        public double LineCalc(PointLatLng point0, PointLatLng point1)
+        {
+            //Function used to calculate the distance between any 2 GPS coordinates that have been passed into this function
+            LineLength = (Math.Sqrt((Math.Pow((point0.Lat - point1.Lat), 2)) + (Math.Pow((point0.Lng - point1.Lng), 2))));
+
+            return LineLength;
+        }
+
+        public double AngleCalc(PointLatLng pointA, PointLatLng pointB, PointLatLng pointX)
+        {
+            //Function used to calculate the angle at pointX when passed any 3 points. 
+            //Returns the angle in radians.
+            double Linea;
+            double Lineb;
+            double Linex;
+            double Anglex;
+
+            Linex = LineCalc(pointA, pointB);
+            Linea = LineCalc(pointB, pointX);
+            Lineb = LineCalc(pointX, pointA);
+
+            //Using cosine rule of angle X = (sin^-1)a^2+b^2-x^2 / 2ab
+            Anglex = Math.Acos(((Linea*Linea)+(Lineb*Lineb)-(Linex*Linex))/(2*Linea*Lineb));
+
+            return Anglex;
+        }
+
+        public bool PointChecker(PointLatLng pointA, PointLatLng pointB, PointLatLng pointC, PointLatLng pointCheck)
+        {
+            //Function used to check if a point is within a defined area
+            //Breaks the user defined operating area into triangles and checks a grid of points to decide
+            //which points are within the shape, and which are outside. If a point is inside the shape it will add
+            //to the internal list which is used to generate the final list of gps coordinates drones should fly to.
+            double Angle0;
+            double Angle1;
+            double Angle2;
+            double TotalAngle;
+            bool result;
+
+            Angle0 = AngleCalc(pointA, pointB, pointCheck);
+            Angle1 = AngleCalc(pointB, pointC, pointCheck);
+            Angle2 = AngleCalc(pointA, pointC, pointCheck);
+
+            TotalAngle = Angle0 + Angle1 + Angle2;
+
+            //If the total angle between the point and 3 of the user defined points is ~=360degrees, this means the point is inside the shape, otherwise it is outside.
+            if ((TotalAngle <= (2*Math.PI + 0.1)) && (TotalAngle >= (2*Math.PI - 0.1)))
+            {
+                result = true;
+            }
+            else result = false;
+
+
+            return result;
+        }
+    }
+
+}
